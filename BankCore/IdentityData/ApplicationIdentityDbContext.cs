ca1f3ceda88d74491fc3e0b2a9238b67c08e4cec
Victor Grimschöld <31439935+CarlVictorGrimschold@users.noreply.github.com>
2025-03-16 22:44:57 +0100

    Added BankCore project

---
+++ BankCore/IdentityData/ApplicationIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BankCore.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankCore.IdentityData
+{
+ 
+    public class ApplicationIdentityDbContext : IdentityDbContext 
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+            : base(options)
+        {
+        }
+        // Lägg till din DbSet här, inuti klassen   
+      //  public DbSet<Account> Accounts { get; set; } det här förstörde koden sjukt nog
+
+    }
+}
---
+++ BankCore/Services/CountryService.cs
@@ -0,0 +1,88 @@
+﻿//using BankCore.Data;
+//using BankCore.Data.Models;
+//using Microsoft.Identity.Client;
+//using Microsoft.Owin.Security;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BankCore.Services
+//{
+//    public class CountryService
+//    {
+//        private readonly VictorBankAppContext _dbContext;
+
+//        public CountryService(VictorBankAppContext dbContext)
+//        {
+//           _dbContext = dbContext;
+//        }
+
+//        public List<Customer> GetCustomersByCountry(string countryCode)
+//        {
+
+//        }
+
+
+
+
+
+
+
+//{
+//    var customersByCountry = _dbContext.Customers
+//        .Where(c => c.CountryCode == countryCode)
+//        .ToList();
+//    return customersByCountry;
+//}
+
+
+
+
+//public List<Customer> GetAllCustomers()
+//{
+//    var allCustomer = _dbContext.Customers.ToList();
+//    return allCustomer;
+//}
+//    }
+//}
+
+using BankCore.Data;
+using BankCore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankCore.Services
+{
+    public class CountryService
+    {
+        private readonly VictorBankAppContext _dbContext;
+
+        public CountryService(VictorBankAppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Customer> GetTopWealthyCustomersByCountry(string countryCode)
+        {
+            
+            var wealthiestCustomersByCountry = _dbContext.Customers
+                .Where(c => c.CountryCode == countryCode)
+                .Include(c => c.Dispositions)
+                .ThenInclude(d =>d.Account)
+                .OrderByDescending(c => c.Dispositions
+                .Where(d => d.Type == "OWNER").Sum(d => d.Account.Balance))
+                .Take(10)                
+                .ToList();
+            return wealthiestCustomersByCountry;
+
+        }
+    }
+}
